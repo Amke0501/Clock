1409a4d1185e941253f5260c334c6ad3c6e69029
Amkelwe Lubavu <lubavuamkelwe@gmail.com>
2025-09-28 10:14:19 +0200

    Added AMPM format

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 setInterval(()=> {
+//to repeat function every 1 second
 let timeNow= new Date();
 console.log (timeNow);
 
 let hours= document.getElementById ('hours');
 let minutes= document.getElementById ('minutes');
 let seconds= document.getElementById ('seconds');
+let ampm=document.getElementById('ampm');
+//reference html elements by Id
+
+//added AMPM format
+
+const meridiem =timeNow.getHours ()>12?"PM":"AM";
+ampm.textContent=meridiem;
 
 hours.innerHTML=(timeNow.getHours() <10?"0":"")+timeNow.getHours ();
 minutes.innerHTML=(timeNow.getMinutes()<10?"0":"")+timeNow.getMinutes ();
